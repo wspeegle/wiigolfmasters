d398135a6d2a9f3e3b6b30e25af1ab2c79fde6da
Wills Speegle <wspeegle@gmail.com>
2019-03-31 20:48:27 -0400

    Site updates and improvements

--- WGM/topleaderboard_scripts.js
+++ WGM/topleaderboard_scripts.js
@@ -1,52 +1,21 @@
-        $(document).ready(function()
-        {
+        var maxPlayers = 11;
+        $(window).bind("load",function()
+        { 
            $('#rightButton').click(function() {
             console.log("button press");
            event.preventDefault();
-
-           if($('#leaderboard').scrollLeft() == 0)
-           {
-               $('#leaderboard').scrollLeft(275)
-           }else if($('#leaderboard').scrollLeft() <= 275)
+            if($('#leaderboard').scrollLeft() >= 0 && $('#leaderboard').scrollLeft() < maxPlayers * 325)
             {
-               $('#leaderboard').scrollLeft(550)
+               $('#leaderboard').scrollLeft($('#leaderboard').scrollLeft() + 325);
             }
-            else if($('#leaderboard').scrollLeft() <= 550)
-            {
-               $('#leaderboard').scrollLeft(825)
-            }else if($('#leaderboard').scrollLeft() <= 825)
-            {
-               $('#leaderboard').scrollLeft(1110)
-            }else if($('#leaderboard').scrollLeft() <= 1100)
-            {
-               $('#leaderboard').scrollLeft(1375)
-            }
-            else if($('#leaderboard').scrollLeft() <= 1375)
-            {
-               $('#leaderboard').scrollLeft(1650)
-            } 
         });
 
         $('#leftButton').click(function() {
            event.preventDefault();
-           if($('#leaderboard').scrollLeft() >= 1375)
-           {
-               $('#leaderboard').scrollLeft(1100)
-           }else if($('#leaderboard').scrollLeft() >= 1100)
+           if($('#leaderboard').scrollLeft() >= 0 && $('#leaderboard').scrollLeft() < maxPlayers * 325)
             {
-               $('#leaderboard').scrollLeft(825)
+               $('#leaderboard').scrollLeft($('#leaderboard').scrollLeft() - 325);
             }
-            else if($('#leaderboard').scrollLeft() >= 825)
-            {
-               $('#leaderboard').scrollLeft(550)
-            }else if($('#leaderboard').scrollLeft() >= 550)
-            {
-               $('#leaderboard').scrollLeft(275)
-            }else if($('#leaderboard').scrollLeft() >= 275)
-            {
-               $('#leaderboard').scrollLeft(0)
-            }
-            
         });
       });
 

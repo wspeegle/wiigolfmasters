94116fd0a5f45e61f4c98a176166ae73f5646c85
Dale Holstein <Holstein30@live.com>
2020-02-01 21:33:57 -0500

    input - gso firebase

--- WGM/live/input.js
+++ WGM/live/input.js
@@ -1,12 +1,3 @@
-function testing() {
-    let firstName = document.getElementById('firstName').value;
-    let lastName = document.getElementById('lastName').value;
-    let player = document.getElementById('player').value;
-    let round = document.getElementById('round').value;
-    let score = document.getElementById('score').value;
-    console.log({ firstName, lastName, player, round, score });
-}
-
 // Initialize Cloud Firestore through Firebase
 const config = {
     apiKey: 'AIzaSyB3qmQNbtcpQeLSn3mHsAoYtDxGL-uHL6c',
@@ -23,11 +14,34 @@ firebase.initializeApp(config);
 
 const firestore = firebase.firestore();
 
+function testing() {
+    let firstName = document.getElementById('firstName').value;
+    let lastName = document.getElementById('lastName').value;
+    let player = document.getElementById('player').value;
+    let round = document.getElementById('round').value;
+    let score = document.getElementById('score').value;
+    firestore
+        .collection('players')
+        .add({
+            first_name: firstName,
+            last_name: lastName
+        })
+        .then(function(docRef) {
+            console.log('Document written with ID: ', docRef.id);
+        })
+        .catch(function(error) {
+            console.error('Error adding document: ', error);
+        });
+    console.log({ firstName, lastName, player, round, score });
+}
+
+// Add a new document with a generated id.
+
 firestore
     .collection('players')
     .get()
     .then(querySnapshot => {
         querySnapshot.forEach(doc => {
-            console.log(`${doc.id} => ${doc.data()}`);
+            console.log(`${doc.id} => ${doc.data().first_name}`);
         });
     });

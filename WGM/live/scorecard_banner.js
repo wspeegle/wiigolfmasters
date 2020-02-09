4994dfe63be7b6f53635bb25d77419eabacd4155
Wills Speegle <wspeegle@gmail.com>
2020-02-09 15:24:40 -0500

    Top Leaderboard is mostly working
    need to add scrolling and figure out how to handle cuts

--- WGM/live/scorecard_banner.js
+++ WGM/live/scorecard_banner.js
@@ -4,17 +4,12 @@ function createScorecardBanner(parent_div)
     db = firebase.firestore();
     //first get all the rounds for the latest year
     var current_year = new Date().getFullYear();
-    var rounds;
-    var players;
     getRoundsForYear(current_year).then(function(rounds)
     {
-        
         //then we need to get a list of all the players that played in the first round
         getPlayersForRound(rounds[0]).then(function(players)
         {
-            
-        //then we need to get the total score of all rounds for that year for that player
-
+            //then we need to get the total score of all rounds for that year for that player
             var promises = [];
             for(var i = 0; i < players.length; i++)
             {
@@ -30,14 +25,6 @@ function createScorecardBanner(parent_div)
                 }
                 Promise.all(promises).then(function(players_info)
                 {
-                    console.log("rounds");
-                    console.log(rounds);
-                    console.log("players");
-                    console.log(players);
-                    console.log("player_scores_array")
-                    console.log(player_scores_array);
-                    console.log("players_info");
-                    console.log(players_info);
                     var master_data = {};
                     for(let i = 0; i < players_info.length; i++)
                     {
@@ -49,63 +36,45 @@ function createScorecardBanner(parent_div)
                         var player_id = Object.keys(player_scores_array[i])[0];
                         master_data[player_id]["TOTAL"] = player_scores_array[i][player_id]["TOTAL"];
                         master_data[player_id]["PAR"] = player_scores_array[i][player_id]["PAR"];
-
                     }   
-                    console.log(master_data);
                     //Now that we have the data we need...we can finally build the fucking scorecard tiles
+                    //...but first we need to make sure they are ordered correctly!
+                    //How the hell do we account for missing the cut
+                    var sorted = Object.keys(master_data).sort(function(a,b)
+                    {
+                        return (master_data[a]["TOTAL"]-master_data[b]["TOTAL"])
+                    }).map(function(k)
+                    {
+                         return k;
+                    });
+                    console.log(sorted);
                     var lb_div = document.getElementById(parent_div);
-                    Object.keys(master_data).forEach(function(key){
-                        var wrapper = document.createElement('a');
-                        wrapper.href = '/players/'+key;
-                        wrapper.className = 'leaderboardCard';
-                        var rank_div = document.createElement('div');
-                        rank_div.className = 'rank';
-                        rank_div.innerHTML = '1';
-                        wrapper.appendChild(rank_div);
-                        var pic_div = document.createElement('div');
-                        pic_div.className = 'cardPic';
-                        var img = document.createElement('img');
-                        img.className = 'playerImage';
-                        img.style.width = '60px';
-                        img.style.height = '60px';
-                        img.src = 'test.jpg';
-                        pic_div.appendChild(img);
-                        wrapper.appendChild(pic_div);
-                        var name_wrapper = document.createElement('div');
-                        var name_div = document.createElement('div');
-                        name_div.className = 'cardName';
-                        name_div.innerHTML = master_data[key]["FIRST_NAME"] + " " + master_data[key]["LAST_NAME"];
-                        name_wrapper.appendChild(name_div);
-                        wrapper.appendChild(name_div);
-                        var score_div = document.createElement('div');
-                        score_div.className = 'cardScore';
-                        var score_span = document.createElement('span');
-                        if(master_data[key]["TOTAL"] < master_data[key]["PAR"])
-                            score_span.className = 'under'
-                        score_span.innerHTML = master_data[key]["PAR"] - master_data[key]["TOTAL"];
-                        score_div.appendChild(score_span);
-                        wrapper.appendChild(score_div);
-                        lb_div.appendChild(wrapper);
-                    })
+                    var position_rank = 1;
+                    var position_text = "1";
                     
+                    for(let i = 0; i < sorted.length; i++)
+                    {
+                        if(i > 0 && master_data[sorted[i]]["TOTAL"] == master_data[sorted[i-1]]["TOTAL"])
+                        {
+                            //there is a tie we need to display that
+                            position_text = "T"+position_rank;                            
+                        }else if(i+1 < sorted.length && master_data[sorted[i]]["TOTAL"] == master_data[sorted[i+1]]["TOTAL"] && master_data[sorted[i]]["TOTAL"] != master_data[sorted[i-1]]["TOTAL"])
+                        {//First score of a series of ties
+                            position_rank++;
+                            position_text = "T"+position_rank;
+                            
+                        }else
+                        {
+                            position_rank = i+1;
+                            position_text = position_rank;
+                        }
+                        lb_div.appendChild(createLeaderboardCard(master_data[sorted[i]], sorted[i], position_text));
+                    }
                     
                 });
-
-
             });
-            
-        
-        
-            
         });
-        
     });
-    current_year--;
-    
-    
-        
-    //then we need to find out how many holes were played that year, and calculate the player's score
-
 }
 
 var getRoundsForYear = function(year){
@@ -171,6 +140,18 @@ var getPlayerTotalsforRounds = function(player, rounds)
     });
 }
 
+var getPlayerImage = function(player_id, img)
+{
+    return new Promise(function(resolve, reject)
+    {
+        var storageRef = firebase.storage().ref();
+        storageRef.child('images/players/'+player_id).getDownloadURL().then(function(url)
+        {
+            img.src = url;
+        });
+    });
+}
+
 var getPlayerName = function(player_id)
 {
     return new Promise(function(resolve, reject)
@@ -200,4 +181,53 @@ function totalScore(score_array)
     }
     return total;
 
+}
+
+
+function createLeaderboardCard(player_data, player_id, position)
+{
+    var wrapper = document.createElement('a');
+    wrapper.href = '/players/'+player_id;
+    wrapper.className = 'leaderboardCard';
+    var rank_div = document.createElement('div');
+    rank_div.className = 'rank';
+    rank_div.innerHTML = position;
+    wrapper.appendChild(rank_div);
+    var pic_div = document.createElement('div');
+    pic_div.className = 'cardPic';
+    var img = document.createElement('img');
+    img.className = 'playerImage';
+    img.style.width = '60px';
+    img.style.height = '60px';
+    getPlayerImage(player_id, img);
+    pic_div.appendChild(img);
+    wrapper.appendChild(pic_div);
+    var name_wrapper = document.createElement('div');
+    var name_div = document.createElement('div');
+    name_div.className = 'cardName';
+    name_div.innerHTML = player_data["FIRST_NAME"] + " " + player_data["LAST_NAME"];
+    name_wrapper.appendChild(name_div);
+    wrapper.appendChild(name_wrapper);
+    var score_div = document.createElement('div');
+    score_div.className = 'cardScore';
+    var score_span = document.createElement('span');
+    if(player_data["TOTAL"] < player_data["PAR"])
+    {
+        score_span.className = 'under'
+        score_span.innerHTML = "-" + (player_data["PAR"] - player_data["TOTAL"]);
+    }else if(player_data["TOTAL"] > player_data["PAR"])
+    {
+        score_span.innerHTML = "+" + (player_data["TOTAL"] - player_data["PAR"]);
+        score_span.className = 'over';
+    }
+    else
+    {
+        score_span.innerHTML = "E";
+        score_span.className = 'over';
+    }
+    let holes_played = (player_data["PAR"] / 72)*18;
+    score_div.appendChild(score_span);
+    score_div.innerHTML += "Thru " + holes_played;
+    name_wrapper.appendChild(score_div);
+    return wrapper;
 }

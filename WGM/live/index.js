697b9d416906de8c4ab98527ff856e374471e4a0
Dale Holstein <Holstein30@live.com>
2020-02-12 21:51:14 -0500

    comment updates

--- WGM/live/index.js
+++ WGM/live/index.js
@@ -3,12 +3,20 @@
 // 1        - Dale   - -2    -  13   - -10   - 70 - 66 - 50 - 186
 // 2        - Wills   - -1   -  13   - -9   - 70 - 66 - 51 - 187
 
+// use scorecard_banner.js as guide
+// also utils line 294 (parforScorecard function) to get par for each round
+// total score function above ^^ (284)
+
+// Player object to push all data
+const playerObj = {};
+
 // Grab each player id
 let playersNameAndId = db
     .collection('players')
     .get()
     .then(querySnapshot => {
-        querySnapshot.forEach(doc => console.log(doc.id, ' => ', doc.data()));
+        querySnapshot.forEach((doc, i) => (playerObj[i] = doc.data()));
+        console.log('playerObj' + playerObj);
     });
 // Import all player data using id
 // Insert data into table

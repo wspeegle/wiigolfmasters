ef5a21ee64f12d52625c556f624890a7b61c5168
Dale Holstein <Holstein30@live.com>
2020-02-12 21:17:01 -0500

    initial setup

---
+++ WGM/live/index.js
@@ -0,0 +1,8 @@
+// Table structure
+// Position - Player - Today - Thru - Total - R1 - R2 - R3 - Total
+// 1        - Dale   - -2    -  13   - -10   - 70 - 66 - 50 - 186
+// 2        - Wills   - -1   -  13   - -9   - 70 - 66 - 51 - 187
+
+// Import all player data
+// Insert data into table
+// Sort by TOTAL
